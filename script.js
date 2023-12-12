ab96dc6f43756a3953ee24206ada81e3428e867c
LucasRPorto <lucasrodriguesporto@gmail.com>
2023-12-12 20:09:31 -0300

    feat: (implement-search) - Lucas Porto

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 const containerVideos = document.querySelector(".videos__container");
-const menuContainer = document.querySelector(".superior__secao__container-wrapper");
 const checkboxContainer = document.querySelector(".cabecalho__switch-input");
+const contextoTipo = document.querySelectorAll(".superior__item");
+const pesquisarBtn = document.querySelector(".pesquisar__btn");
 
 const musicaClick = new Audio('./audio/swoosh.mp3');
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos) => 
-    videos.forEach((video) => {
-      containerVideos.innerHTML += `
-        <li class = "videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class = "descricao-video">
-                <img class = "img-canal" src=${video.imagem} alt = "Logo do canal">
-                <h3 class = "titulo-video">${video.titulo}</h3>
-                <p class = "titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-      `;
-    })
-)
+async function buscaVideo(parametro = ""){
+  const api = await fetch(`http://localhost:3000/videos/${parametro}`);
+  const conexaoConvertida = await api.json();
+
+  console.log(conexaoConvertida)
+  return conexaoConvertida;
+}
+
+async function listaVideos( parametro=""){
+
+  const videos = await buscaVideo(parametro);
+  await videos.forEach((video) => {
+    containerVideos.innerHTML += `
+      <li class = "videos__item">
+        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+          <div class = "descricao-video">
+              <img class = "img-canal" src=${video.imagem} alt = "Logo do canal">
+              <h3 class = "titulo-video">${video.titulo}</h3>
+              <p class = "titulo-canal">${video.descricao}</p>
+          </div>
+      </li>
+    `
+  })
+}
+
+listaVideos();
+
+pesquisarBtn.addEventListener('click', (evento) => {
+
+  while(containerVideos.firstChild){
+    containerVideos.removeChild(containerVideos.firstChild);
+  }
+  const dataPesquisa = document.querySelector("[data-pesquisa]").value;
+  evento.preventDefault();
+  console.log(dataPesquisa);
+  listaVideos(`?q=${dataPesquisa}`);
+})
 
 checkboxContainer.addEventListener('change', () =>{
   musicaClick.play();
